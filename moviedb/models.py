2ee68ac1a131f3ddf124fee8096e821bf8ffb469
Marc Egli <frog32@me.com>
2010-07-19 23:44:46 +0200

    removed unused import

--- moviedb/models.py
+++ moviedb/models.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with morgainemoviedb.  If not, see <http://www.gnu.org/licenses/>.
 
-from moviedb.custom_models import RenameFilesModel
 from moviedb.conf import settings
 from moviedb.utils import *
 from django.db import models
